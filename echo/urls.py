8768d1257c20dc5c4484a48915abf5d311dd307f
Kom Sihon <rsihon@gmail.com>
2018-11-06 17:13:02 +0100

    URL renamed according to ikwen scheme

--- echo/urls.py
+++ echo/urls.py
@@ -6,10 +6,10 @@ from echo.views import SMSCampaign, SMSHistory, SMSBundle, MailCampaign, MailHis
 urlpatterns = patterns(
     '',
     url(r'^sms/$', permission_required('echo.ik_messaging_campaign')(SMSCampaign.as_view()), name='sms_campaign'),
-    url(r'^sms_hist/$', permission_required('echo.ik_messaging_campaign')(SMSHistory.as_view()), name='sms_hist'),
-    url(r'^sms_bundle/$', permission_required('echo.ik_messaging_campaign')(SMSBundle.as_view()), name='sms_bundle'),
+    url(r'^smsHistory/$', permission_required('echo.ik_messaging_campaign')(SMSHistory.as_view()), name='sms_history'),
+    url(r'^smsBundles/$', permission_required('echo.ik_messaging_campaign')(SMSBundle.as_view()), name='sms_bundle'),
     url(r'^mail/$', permission_required('echo.ik_messaging_campaign')(MailCampaign.as_view()), name='mail_campaign'),
-    url(r'^mail_hist/$', permission_required('echo.ik_messaging_campaign')(MailHistory.as_view()), name='mail_hist'),
-    url(r'^mail_bundle/$', permission_required('echo.ik_messaging_campaign')(MailBundle.as_view()), name='mail_bundle'),
+    url(r'^mailHistory/$', permission_required('echo.ik_messaging_campaign')(MailHistory.as_view()), name='mail_hist'),
+    url(r'^mailBundles/$', permission_required('echo.ik_messaging_campaign')(MailBundle.as_view()), name='mail_bundle'),
     url(r'^csv_upload/$', csv_uploader, name='csv_uploader'),
 )
