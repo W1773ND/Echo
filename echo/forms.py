50da72dd638b3779ca6db31ae37d39a78f80e12b
Kom Sihon <rsihon@gmail.com>
2019-05-03 21:41:45 +0100

    forms.py removed

--- echo/forms.py
+++
@@ -1,9 +0,0 @@
-# from django import forms
-#
-# from echo.models import CSVFile
-#
-#
-# class CSVFileForm(forms.ModelForm):
-#     class Meta:
-#         model = CSVFile
-#         fields = ('file',)
